1eb20cc113675c0725ee64a11c0ae48dd32b2604
Henok <henatan99@gmail.com>
2020-10-31 18:55:09 +0300

    Adds background to the body, adds background to the concert-invite section, change background to the events article elements
    - A purely custom made background csv image is created by online tool and us
    as background for teh body. It has good contrast with the texts.
    
    - A purely custom made background is created by online tool and used as
    background for the concert invite section. The image is representative of
    the concert. Slide animation is added to the background image.

---
+++ assets/images/untitled - Vectr_files/890.js
@@ -0,0 +1,64 @@
+BAP.copyJSON({
+  "data": {
+    "icon_position": "top-right",
+    "default_icon": "_us",
+    "mobile_in_app_url": "",
+    "default_footer": "Privacy Controls by Evidon, Inc.",
+    "icon_display": "normal",
+    "icon_grayscale": 30,
+    "container_opacity": 60,
+    "offset_x": 0,
+    "offset_y": 0,
+    "generic_icon": false,
+    "icon_delay": 0,
+    "nid": 890,
+    "nwid": null,
+    "aid": 412,
+    "icid": null,
+    "skip_L2": false,
+    "behavioral": "custom",
+    "generic_text": "This ad was selected for you based on your browsing activity. NextRoll, Inc. (and its business divisions, AdRoll and RollWorks) serve ads that aim to be relevant to you. To opt out of receiving personalized ads served by NextRoll, click the NextRoll opt-out link below.",
+    "adv_name": "NextRoll, Inc.",
+    "adv_msg": "NextRoll, Inc. opt-out",
+    "adv_logo": "http://c.evidon.com/a/ui/logos/412/fa9d926a290d4ec0be88cdf5d6b7f84e.jpg",
+    "adv_link": "http://app.adroll.com/optout?utm_source=evidon&utm_medium=AdChoices&utm_campaign=opt-out",
+    "mobile_message": "Tap to edit advertising preferences",
+    "display_mobile_overlay": false,
+    "mobile_advertiser_logo_url": "",
+    "default_icon_text": "AdChoices",
+    "default_generic1": "This ad has been matched to your interests. It was selected for you based on your browsing activity.",
+    "default_generic2": "This ad may have been matched to your interests based on your browsing activity.",
+    "default_generic3": "helped",
+    "default_generic4": "determine that you might be interested in an ad like this.",
+    "default_generic5": "select this ad for you.",
+    "default_generic6": "selected this ad for you.",
+    "default_link1": "More information & opt-out options",
+    "default_link2": "What is interest based advertising",
+    "default_link3": "Learn about your choices",
+    "ecid": null,
+    "hide_wi": false,
+    "hide_cl": false,
+    "server": [
+      {
+        "name": "NextRoll, Inc"
+      }
+    ],
+    "message_properties": {
+      "skip_L2_gb": true,
+      "translation_gb": {
+        "icon": "_us",
+        "icon_text": "AdChoices"
+      },
+      "skip_L2_ca": true,
+      "translation_ca": {
+        "icon": "_us",
+        "icon_text": "AdChoices"
+      },
+      "skip_L2_ie": true,
+      "translation_ie": {
+        "icon": "_us",
+        "icon_text": "AdChoices"
+      }
+    }
+  }
+});
